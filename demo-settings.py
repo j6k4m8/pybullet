ddb47859de9e0dbca39c84ccacd73f52ddb5046b
Jordan Matelsky <j6k4m8@gmail.com>
2015-07-09 16:28:48 -0400

    Update demo-settings.py

--- demo-settings.py
+++ demo-settings.py
@@ -1,6 +1,5 @@
 settings = {
     "PUSHBULLET_API_KEY":                "",
     "PUSHBULLET_USER_IDEN":              "user_iden (beginning of device iden)",
-    "PUSHBULLET_SMS_IDEN":               "device_iden",
-    "SEND_PUSHBULLET_NOTIFICATIONS":     False
+    "PUSHBULLET_SMS_IDEN":               "device_iden"
 }
