e9ddee84ed6c4d8f6a8e3fdc25a05673212e84c1
Jordan <j6k4m8@gmail.com>
2015-07-09 16:11:58 -0400

    Fix bad appending to .contacts file

--- contacts.py
+++ contacts.py
@@ -3,13 +3,13 @@ import sys
 _contacts = [line.strip() for line in open(".contacts", 'r')]
 
 contacts = {}
-for c in range(len(_contacts)):
+for c in range(len(_contacts) - 1):
     con = _contacts[c].split()
     contacts[con[0]] = con[1]
 
 def add_contact(name, number):
     with open('.contacts', 'a') as file:
-        file.write("\n" + name + "\t" + number)
+        file.write(name + "\t" + number + "\n")
         contacts[name] = number
 
 def get_number(name):
